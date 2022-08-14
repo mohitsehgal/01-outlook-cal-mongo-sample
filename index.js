8b1eaa56f6c829700a452fe8adca765bc211d903
Mohit Sehgal <sehgal.mohit29@gmail.com>
2022-08-14 13:41:54 +0530

    serve html on home

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 const config = require("./config");
 const port = config.PORT || 3000;
+const path = require("path");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(port, () => {
